5e762458767814466e02380ba74657dea0d9842a
haoleday <1131185292@qq.com>
2020-09-28 18:32:35 +0800

    ss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-27 18:20:48
+ * @LastEditTime: 2020-09-28 13:26:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \zt\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
@@ -12,6 +20,30 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-undef":'off',
+    "generator-star-spacing": "off",
+    "no-mixed-operators": 0,
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        "singleline": 5,
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": false
+        }
+      }
+    ],
+    "vue/attribute-hyphenation": 0,
+    "vue/html-self-closing": 0,
+    "vue/component-name-in-template-casing": 0,
+    "vue/html-closing-bracket-spacing": 0,
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/no-unused-components": 0,
+    "vue/multiline-html-element-content-newline": 0,
+    "vue/no-use-v-if-with-v-for": 0,
+    "vue/html-closing-bracket-newline": 0,
+    "vue/no-parsing-error": 0,
+    "no-console": 0
   }
 }
